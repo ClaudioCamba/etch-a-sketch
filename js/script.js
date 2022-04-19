d45a8a23b27f186c1c0e3846ded539692c293063
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-19 17:14:15 +0100

    Add randomColor() to generate random rgb for individual sqaure hovered

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let domElements = {
     divNum: 20
 }
 
+// Create and append grids
 function appendGrid(num) {
     let main = document.createElement('div');
     gridNum = num * num;
@@ -14,7 +15,7 @@ function appendGrid(num) {
         let div = document.createElement('div');
         // div.textContent = i;
         main.appendChild(div);
-        console.log(i)
+        // console.log(i)
     }
 
     // Place all in html body
@@ -24,40 +25,67 @@ function appendGrid(num) {
     hoverEffect();
 }
 
+// Add class to change color of div when hovered
 function hoverEffect() {
-    // Change color of div when hovered
     let divColor = document.querySelectorAll('.container div');
 
     function hoveredDiv(e) {
-        this.classList.add('color');
-        console.log(this);
+        if (this.classList.contains('col')) {
+            console.log(this.style.backgroundColor)
+        } else {
+            this.classList.add('col');
+            this.style.backgroundColor = randomColor();
+        }
     }
 
     divColor.forEach(divColor => divColor.addEventListener('mouseover', hoveredDiv));
 }
 
+// Replay button to prompt user and reset game
 domElements.btn.addEventListener('click', function () {
     let gridNum = prompt('Enter number from 1-100');
     domElements.divNum = gridNum;
 
-    console.log(domElements.divNum)
-
     document.querySelector('.container').remove();
     appendGrid(domElements.divNum);
-    hoverEffect();
-
 });
 
 // Dynamic width for div children
 function dynamicDivWidth() {
     let parentElem = document.querySelector('.container'),
-        childElem = document.querySelectorAll('.container div');
+        childElem = document.querySelectorAll('.container div'),
+        widthViewP = viewportWidth();
+    let vwVal = 100 * (Math.floor(parentElem.offsetWidth / domElements.divNum) - 1) / widthViewP + 'vw';
 
     for (const child of childElem) {
-        child.style.flexBasis = Math.floor((parentElem.offsetWidth / domElements.divNum) - 1) + 'px'; // calculate the width for each div using input number
+        child.style.flexBasis = vwVal; // calculate the width for each div using input number
+        // console.log(vwVal);
     }
 }
 
+// Get viewport to calculate individual grid width
+function viewportWidth() {
+    var win = window,
+        doc = document,
+        docElem = doc.documentElement,
+        body = doc.getElementsByTagName('body')[0],
+        x = win.innerWidth || docElem.clientWidth || body.clientWidth;
+    return x;
+}
+
+// Generate random rgb color
+function randomColor() {
+    let rgb = {
+        r: Math.round(Math.random() * 255),
+        g: Math.round(Math.random() * 255),
+        b: Math.round(Math.random() * 255),
+        color: function () {
+            return 'rgb(' + this.r + ',' + this.g + ',' + this.b + ')';
+        }
+    };
+
+    return rgb.color();
+}
 
 // Add replay button to page 
 domElements.btn.textContent = 'Replay';
@@ -67,7 +95,5 @@ domElements.body.appendChild(domElements.btn);
 appendGrid(domElements.divNum);
 
 
-
-// Limi
 
 
